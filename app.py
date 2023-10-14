8a42b2c73ce3d968469d6d2bde591fb66c38b9b8
sddosx <moudni3@gmail.com>
2023-10-14 16:07:33 -0500

    weapon detect, pose, tracking

--- app.py
+++ app.py
@@ -1,12 +1,31 @@
+"""
+    -----------------------------------------------------------
+    ECE441 - Illinois Institute of Technology
+    Author: Alae Moudni
+    Email: amoudni@hawk.iit.edu
+    Date: October 2023
+    
+    Description:
+    This code is a part of the ECE441 project. Unauthorized copying,
+    distribution, or any form of plagiarism without explicit permission is
+    strictly prohibited. All rights reserved.
+    -----------------------------------------------------------
+"""
+import os
+os.environ['KMP_DUPLICATE_LIB_OK']='True'
+
 from fastapi import FastAPI, Request
 from fastapi.responses import StreamingResponse
 from fastapi.templating import Jinja2Templates
 from fastapi.staticfiles import StaticFiles
 import cv2
-from ultralytics import YOLO
 import logging
-from starlette.concurrency import run_in_threadpool
 import random
+from src.architecture import YOLOModel
+import asyncio
+from starlette.concurrency import run_in_threadpool
+import threading
+from queue import Queue
 
 # Setup logging
 logging.basicConfig(level=logging.INFO)
@@ -16,129 +35,51 @@ app = FastAPI()
 app.mount("/static", StaticFiles(directory="static"), name="static")
 templates = Jinja2Templates(directory="templates")
 
-# Initialize camera
-camera = cv2.VideoCapture(0)
-if not camera.isOpened():
-    logging.error("Cannot open camera")
-    exit()
-
-# Initialize model
-try:
-    model = YOLO("weapons.pt")
-except Exception as e:
-    logging.error(f"Error initializing model: {str(e)}")
-    model = None
-
-# Warmup model if it's available
-def warmup(model):
-    if model:
-        logging.info("Warming up model")
-        try:   
-            frame = cv2.imread("bus.jpg")
-            _ = model(frame)
-            logging.info("Model warmed up")
-        except Exception as e:
-            logging.error(f"Error during warmup: {str(e)}")
-    else:
-        logging.error("Model is not available. Skipping warmup.")
-
-warmup(model)
-
-def detect(model, frame):
-    if model:
-        try:
-            results = model(frame)
-            logging.info(results)
-            draw_results(frame, results)  
-            #return results
-        except Exception as e:
-            logging.error(f"Error during detection: {str(e)}")
-            return None
-    else:
-        logging.error("Model is not available. Skipping detection.")
-        return None
-
-def draw_results(frame, results):
-    # Check whether results are not None and non-empty
-    if results and len(results) > 0:
-        # Extract the Results object
-        result_obj = results[0]
-        
-        # Log some of its attributes for debugging
-        logging.info(f"Result object attributes: {dir(result_obj)}")
-        
-        # Check if 'boxes' attribute exists and is not None
-        if hasattr(result_obj, 'boxes') and result_obj.boxes is not None:
-            # Try to access the xyxy attribute
-            try:
-                # Accessing boxes and converting them to numpy array
-                boxes = result_obj.boxes.xyxy[0].cpu().numpy()
-            except AttributeError as e:
-                logging.error(f"Cannot access box coordinates: {str(e)}")
-                return
-            
-            # Log the extracted 'boxes' data
-            logging.info(f"Extracted boxes: {boxes}")
-            
-            # If we successfully obtained boxes, draw them
-            if boxes is not None:
-                if len(boxes.shape) == 1:  # single box
-                    xyxy, conf, cls = boxes[:4], boxes[4], boxes[5]
-                    label = f'{model.names[int(cls)]} {conf:.2f}'
-                    plot_one_box(xyxy, frame, label=label, color=(255, 0, 0), line_thickness=2)
-                elif len(boxes.shape) == 2:  # multiple boxes
-                    for box in boxes:
-                        xyxy, conf, cls = box[:4], box[4], box[5]
-                        label = f'{model.names[int(cls)]} {conf:.2f}'
-                        plot_one_box(xyxy, frame, label=label, color=(255, 0, 0), line_thickness=2)
-        else:
-            logging.warning("No detection boxes found in the results.")
-
-
-
-def plot_one_box(xyxy, img, color=None, label=None, line_thickness=None):
-    tl = line_thickness or round(0.002 * (img.shape[0] + img.shape[1]) / 2) + 1
-    color = color or [random.randint(0, 255) for _ in range(3)]
-    c1, c2 = (int(xyxy[0]), int(xyxy[1])), (int(xyxy[2]), int(xyxy[3]))
-    cv2.rectangle(img, c1, c2, color, thickness=tl, lineType=cv2.LINE_AA)
-    if label:
-        tf = max(tl - 1, 1)  
-        t_size = cv2.getTextSize(label, 0, fontScale=tl / 3, thickness=tf)[0]
-        c2 = c1[0] + t_size[0], c1[1] - t_size[1] - 3
-        cv2.rectangle(img, c1, c2, color, -1, cv2.LINE_AA)  
-        cv2.putText(img, label, (c1[0], c1[1] - 2), 0, tl / 3, [225, 255, 255], thickness=tf, lineType=cv2.LINE_AA)
-
-@app.on_event("shutdown")
-def shutdown_event():
-    logging.info("Releasing camera")
-    camera.release()
+# Initialize YOLO model
+yolo_model = YOLOModel("src/weights/yolov8s-pose.pt")
 
 @app.get("/")
 async def index(request: Request):
     return templates.TemplateResponse("index.html", {"request": request})
 
-async def generate():
-    while True:
-        try:
-            success, frame = await run_in_threadpool(camera.read)
-            if not success:
-                logging.error("Failed to read frame")
-                break
-            
-            _ = await run_in_threadpool(detect, model, frame)  
-            
-            _, jpeg = await run_in_threadpool(cv2.imencode, '.jpg', frame)
-            
-            if not _:
-                logging.error("Failed to encode frame")
-                break
+class VideoCamera:
+    def __init__(self):
+        self.video = cv2.VideoCapture(0)
+        self.clients = []
+        self.frame = None
+        thread = threading.Thread(target=self.get_frame, args=())
+        thread.daemon = True
+        thread.start()
 
-            yield (b'--frame\r\n'
-                   b'Content-Type: image/jpeg\r\n\r\n' + jpeg.tobytes() + b'\r\n\r\n')
-        except Exception as e:
-            logging.error(f"An error occurred in generate(): {str(e)}")
-            break
+    def get_frame(self):
+        while True:
+            success, image = self.video.read()
+            if not success:
+                break
+            #image = yolo_model.detect_and_track_objects2(image)
+            #image = yolo_model.detect_objects(image)
+            image = yolo_model.detect_pose(image)
+            ret, buffer = cv2.imencode('.jpg', image)
+            frame = buffer.tobytes()
+            self.frame = (b'--frame\r\n'
+                          b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n')
+            self.broadcast()
+
+    def broadcast(self):
+        for q in self.clients:
+            q.put(self.frame)
+
+    def get_stream(self):
+        q = Queue(maxsize=1)
+        self.clients.append(q)
+        try:
+            while True:
+                yield q.get()
+        finally:
+            self.clients.remove(q)
+
+camera = VideoCamera()
 
 @app.get("/video_feed")
 async def video_feed():
-    return StreamingResponse(generate(), media_type="multipart/x-mixed-replace; boundary=frame")
+    return StreamingResponse(camera.get_stream(), media_type="multipart/x-mixed-replace;boundary=frame")
---
+++ src/architecture.py
@@ -0,0 +1,275 @@
+from ultralytics import YOLO
+import cv2
+import math
+from collections import defaultdict
+import numpy as np
+import torch
+
+track_history = defaultdict(lambda: [])
+
+SKELETON = [
+    (0, 1), (0, 2), (1, 3), (2, 4),         # Head
+    (5, 6), (5, 7), (6, 8), (7, 9), (8, 10), # Arms
+    (5, 11), (6, 12), (11, 12),             # Shoulders and Hip
+    (11, 13), (12, 14), (13, 15), (14, 16)  # Legs
+]
+
+
+COLORS = [
+    (0, 0, 255), (0, 127, 255), (0, 255, 255), (0, 255, 0),
+    (255, 127, 0), (255, 0, 0), (255, 0, 127), (255, 0, 255),
+    (127, 0, 255), (0, 0, 255), (127, 255, 0), (255, 255, 0),
+    (0, 127, 255), (0, 255, 127), (127, 0, 255), (255, 127, 0)
+]
+
+
+
+class YOLOModel:
+    
+    def __init__(self, weights_path):
+        self.model = None
+        self.class_names = ['Grenade', 'Gun', 'Knife', 'Pistol', 'handgun', 'rifle']
+        self.load_model(weights_path)
+        self.device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
+    
+    def load_model(self, weights_path):
+        try:
+            self.model = YOLO(weights_path)
+        except Exception as e:
+            print(f"Error initializing model: {str(e)}")
+
+    def warmup(self):
+        if self.model:
+            print("Warming up model")
+            try:   
+                frame = cv2.imread("test.jpg")
+                _ = self.model(frame)
+                print("Model warmed up")
+            except Exception as e:
+                print(f"Error during warmup: {str(e)}")
+                exit()
+        else:
+            print("Model is not available. Skipping warmup.")
+
+    def detect_objects(self, image):
+        results = self.model(image, stream=True)
+        for r in results:
+            boxes = r.boxes
+            for box in boxes:
+                x1, y1, x2, y2 = box.xyxy[0]
+                x1, y1, x2, y2 = int(x1), int(y1), int(x2), int(y2)
+                conf = math.ceil((box.conf[0] * 100)) / 100
+                cls = int(box.cls[0])
+                class_name = self.class_names[cls]
+                label = f'{class_name}{conf}'
+                t_size = cv2.getTextSize(label, 0, fontScale=1, thickness=2)[0]
+                c2 = x1 + t_size[0], y1 - t_size[1] - 3
+                if class_name == 'Pistol':
+                    color = (0, 204, 255)
+                elif class_name == "Knife":
+                    color = (222, 82, 175)
+                elif class_name == "handgun":
+                    color = (0, 149, 255)
+                else:
+                    color = (85, 45, 255)
+                if conf > 0.5:
+                    cv2.rectangle(image, (x1, y1), (x2, y2), color, 3)
+                    cv2.rectangle(image, (x1, y1), c2, color, -1, cv2.LINE_AA)  # filled
+                    cv2.putText(image, label, (x1, y1 - 2), 0, 1, [255, 255, 255], thickness=1, lineType=cv2.LINE_AA)
+        return image
+    
+    def detect_and_track_objects(self, image):
+        # Perform object detection and tracking
+        results = self.model.track(image, persist=True)  # Note the `track` method
+        for r in results:
+            boxes = r.boxes
+            for box in boxes:
+                x1, y1, x2, y2 = box.xyxy[0]
+                x1, y1, x2, y2 = int(x1), int(y1), int(x2), int(y2)
+                conf = math.ceil((box.conf[0] * 100)) / 100
+                cls = int(box.cls[0])
+                class_name = self.class_names[cls]
+                label = f'{class_name}{conf}'
+                t_size = cv2.getTextSize(label, 0, fontScale=1, thickness=2)[0]
+                c2 = x1 + t_size[0], y1 - t_size[1] - 3
+                color = self.get_color(class_name)
+                if conf > 0.5:
+                    cv2.rectangle(image, (x1, y1), (x2, y2), color, 3)
+                    cv2.rectangle(image, (x1, y1), c2, color, -1, cv2.LINE_AA)  # filled
+                    cv2.putText(image, label, (x1, y1 - 2), 0, 1, [255, 255, 255], thickness=1, lineType=cv2.LINE_AA)
+        return image
+    
+    def detect_and_track_objects2(self, image):
+        results = self.model.track(image, persist=True)
+
+        boxes = results[0].boxes.xywh.cpu()
+
+        #check tensor size if size (0, 4) then return image
+        if boxes.size(0) == 0:
+            return image
+        try:
+            track_ids = results[0].boxes.id.int().cpu().tolist()
+
+            annotated_image = results[0].plot()
+
+            for box, track_id in zip(boxes, track_ids):
+                x, y, w, h = box
+                track = track_history[track_id]
+                track.append((float(x), float(y)))
+                if len(track)>30:
+                    track.pop(0)
+
+                points = np.hstack(track).astype(np.int32).reshape(-1, 1, 2)
+                cv2.polylines(annotated_image, [points], isClosed=False, color=self.get_color(self.class_names[0]), thickness=10)
+
+            return annotated_image
+        except Exception as e:
+            print("No object detected")
+            return image
+        
+    def detect_pose(self, image):
+        #self.model.eval()  # Ensure the model is in evaluation mode
+
+        # Convert image to suitable format
+        input_tensor = self._prepare_image(image)
+
+        # Forward pass through the model
+        with torch.no_grad():
+            outputs = self.model(input_tensor)
+
+        # Process outputs 
+        keypoints = self._process_outputs(outputs, image.shape)
+
+        # Draw keypoints and skeletons on the image
+        annotated_image = self._draw_poses(image, keypoints)
+
+        return annotated_image
+
+    def _prepare_image(self, image):
+        # Convert the image from BGR to RGB
+        image_rgb = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
+        
+        # Convert the image to PyTorch tensor
+        input_tensor = torch.from_numpy(image_rgb).permute(2, 0, 1).float()
+
+        # Add a batch dimension and normalize
+        input_tensor = input_tensor.unsqueeze(0) / 255.0  
+
+        # Move tensor to the appropriate device
+        input_tensor = input_tensor.to(self.device)  
+
+        return input_tensor
+
+    def _process_outputs(self, outputs, original_shape):
+        # Ensure outputs is a numpy array, if it's a list perhaps take an element.
+        if isinstance(outputs, list):
+            outputs = outputs[0].keypoints.xy
+        keypoints = outputs.cpu().numpy().squeeze()
+        
+        # Handling all zero keypoints
+        if np.all(keypoints == 0):
+            print("No keypoints detected.")
+            return None
+        return keypoints
+
+
+    # def _draw_poses(self, image, keypoints):
+    #     if keypoints is None or len(keypoints) == 0:
+    #         print("No keypoints to draw.")
+    #         return image
+        
+    #     keypoints = np.array(keypoints)
+    #     print(f"Keypoints Shape: {keypoints.shape}")
+    #     print(f"Keypoints: {keypoints}")
+
+    #     # Loop through all detected persons
+    #     for person_keypoints in keypoints:
+    #         # Draw keypoints
+    #         for keypoint in person_keypoints:
+    #             print(f"Processing keypoint: {keypoint}, type: {type(keypoint)}")
+    #             try:
+    #                 x, y = keypoint
+    #                 if x > 0 and y > 0:
+    #                     image = cv2.circle(image, (int(x), int(y)), 5, (0, 0, 255), -1)
+    #             except ValueError as e:
+    #                 print(f"Failed to unpack keypoint {keypoint}: {e}. Skipping this keypoint.")
+    #                 continue
+            
+    #         # Draw skeleton
+    #         for i, (start_p, end_p) in enumerate(SKELETON):
+    #             try:
+    #                 x1, y1 = person_keypoints[start_p]
+    #                 x2, y2 = person_keypoints[end_p]
+    #             except ValueError as e:
+    #                 print(f"Error unpacking keypoint: {e}.")
+    #                 print(f" - Problematic keypoints: {person_keypoints[start_p]} and {person_keypoints[end_p]}.")
+    #                 print(f" - Full keypoints array: {keypoints}.")
+    #                 continue
+                
+    #             # Only draw the skeleton if both points are valid
+    #             if x1 > 0 and y1 > 0 and x2 > 0 and y2 > 0:
+    #                 image = cv2.line(image, (int(x1), int(y1)), (int(x2), int(y2)), COLORS[i], 2)
+    #     return image
+
+    def _draw_poses(self, image, keypoints):
+        if keypoints is None or len(keypoints) == 0:
+            print("No keypoints to draw.")
+            return image
+        
+        keypoints = np.array(keypoints)
+        print(f"Keypoints Shape: {keypoints.shape}")
+        print(f"Keypoints: {keypoints}")
+
+        # If keypoints array is 3-dimensional, treat it as [person][keypoint][coordinates]
+        if len(keypoints.shape) == 3:
+            for person_keypoints in keypoints:
+                image = self._draw_single_person(image, person_keypoints)
+        # If keypoints array is 2-dimensional, treat it as [keypoint][coordinates]
+        elif len(keypoints.shape) == 2:
+            image = self._draw_single_person(image, keypoints)
+        else:
+            print("Unexpected keypoints shape, skipping drawing.")
+        
+        return image
+
+    def _draw_single_person(self, image, keypoints):
+        # Draw keypoints
+        for keypoint in keypoints:
+            print(f"Processing keypoint: {keypoint}, type: {type(keypoint)}")
+            try:
+                x, y = keypoint
+                if x > 0 and y > 0:
+                    image = cv2.circle(image, (int(x), int(y)), 5, (0, 0, 255), -1)
+            except ValueError as e:
+                print(f"Failed to unpack keypoint {keypoint}: {e}. Skipping this keypoint.")
+                continue
+        
+        # Draw skeleton
+        for i, (start_p, end_p) in enumerate(SKELETON):
+            try:
+                x1, y1 = keypoints[start_p]
+                x2, y2 = keypoints[end_p]
+            except ValueError as e:
+                print(f"Error unpacking keypoint: {e}.")
+                print(f" - Problematic keypoints: {keypoints[start_p]} and {keypoints[end_p]}.")
+                print(f" - Full keypoints array: {keypoints}.")
+                continue
+            
+            # Only draw the skeleton if both points are valid
+            if x1 > 0 and y1 > 0 and x2 > 0 and y2 > 0:
+                image = cv2.line(image, (int(x1), int(y1)), (int(x2), int(y2)), COLORS[i], 2)
+
+        return image
+
+
+
+    def get_color(self, class_name):
+        # Define color code based on class_name
+        if class_name == 'Pistol':
+            return (0, 204, 255)
+        elif class_name == "Knife":
+            return (222, 82, 175)
+        elif class_name == "handgun":
+            return (0, 149, 255)
+        else:
+            return (85, 45, 255)
